f07cbca1740e3e89b80fb386cef09f237134021c
RoselineDC <roseline.danga@gmail.com>
2026-05-07 10:22:16 +0200

    fix mobile resolution

--- light-on-campus/app/page.tsx
+++ light-on-campus/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="w-full max-w-screen-2xl mx-auto px-4">
+      <main >
         <Hero />
         <Details />
         <Expect />
